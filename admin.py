4f125941cf11ee23b7f0892ba360a2293f0bbc1d
KwaziCoder <kwazi.coder@gmail.com>
2023-03-01 21:51:47 +0300

    add stickers

--- admin.py
+++ admin.py
@@ -6,33 +6,38 @@ from excelParser import parse_excel
 ADMINS = [763665227]
 
 
-async def auth(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def auth(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
     if update.message.from_user.id in ADMINS:
-        print("Admin")
-        context.user_data["is_admin"] = True
+        return True
     else:
-        print("Not admin")
-        context.user_data["is_admin"] = False
+        await context.bot.send_message(update.effective_chat.id,
+                                       "Отказано в доступе! У вас нет прав закачивание файлов!")
+        return False
 
 
 async def upload_doc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print("Document")
-    if update.message.from_user.id in ADMINS:
-        file = await context.bot.get_file(update.message.document)
-        await file.download_to_drive('./assets/files/books.xlsx')
-        await parse_excel(update, context)
-    else:
-        await context.bot.send_message(update.effective_chat.id, "Отказано в доступе! Авторизируйтесь как администратор!")
+
+    if await auth(update, context):
+        excel = update.message.document
+        if excel.file_name == "books.xlsx":
+            file = await context.bot.get_file(excel)
+            await file.download_to_drive('./assets/files/books.xlsx')
+            await parse_excel(update, context)
+            await context.bot.send_message(update.effective_chat.id, "Файл 'books.xlsx' загружен!")
 
 
 async def upload_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print("Image")
-    if update.message.from_user.id in ADMINS:
-        file = await context.bot.get_file(update.message.document.file_id)
-        await file.download_to_drive(f'./assets/images/{update.message.document.file_name}')
-    else:
-        await context.bot.send_message(update.effective_chat.id, "Отказано в доступе! Авторизируйтесь как администратор!")
+
+    if await auth(update, context):
+        image = update.message.document
+        file = await context.bot.get_file(image.file_id)
+        await file.download_to_drive(f'./assets/images/{image.file_name}')
+        await context.bot.send_message(update.effective_chat.id, f"Файл '{image.file_name}' успешно загружен!")
 
 
+async def define_sticker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update.message.sticker.file_id)
 
 
--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, Bot, MenuButto
     BotCommandScopeAllChatAdministrators, BotCommandScope
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 
-from admin import upload_doc, auth, upload_image
+from admin import upload_doc, auth, upload_image, define_sticker
 from books import book_find_process
 from excelParser import parse_excel
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.set_my_commands(
-        [BotCommand("start", "Подобрать книги")])
-
     if "data" not in context.bot_data:
         await parse_excel(update, context)
 
@@ -21,6 +18,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     context.user_data["polling_in_progress"] = True
 
+    await context.bot.send_sticker(update.effective_chat.id, "CAACAgUAAxkBAAIEhGP_myRO81SMdRKquFUQdvRu7zs6AAKCAwAC6QrIA4xZA7HpW8S3LgQ")
     await context.bot.send_message(update.effective_chat.id, "Привет! Меня зовут Федя. Я кибер-сотрудник библиотеки-филиал №15. Не знаешь что бы почитать? Давай я задам тебе пару наводящих вопросов?")
 
     ages = context.bot_data["data"].keys()
@@ -46,16 +44,18 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     books_handler = MessageHandler(filters.Regex(r"\w+"), book_find_process)
 
+    stickers_handler = MessageHandler(filters.Sticker.ALL, define_sticker)
+
     upload_doc_handler = MessageHandler(filters.Document.FileExtension("xlsx"), upload_doc)
     upload_image_handler = MessageHandler(filters.Document.IMAGE, upload_image)
-    # auth_handler = CommandHandler('auth', auth)
 
     application.add_handler(start_handler)
 
     application.add_handler(books_handler)
 
+    application.add_handler(stickers_handler)
+
     application.add_handler(upload_doc_handler)
     application.add_handler(upload_image_handler)
-    # application.add_handler(auth_handler)
 
     application.run_polling()
